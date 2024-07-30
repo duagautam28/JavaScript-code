1fab40425c6ef11e9b3b96be985c49bd9831fa54
duagautam28 <147668577+duagautam28@users.noreply.github.com>
2024-07-30 10:58:48 +0000

    function

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,28 @@
+// var b=10
+// let a=20
+// if(true){
+//     let a=100
+//     let b=300
+// }
+// console.log(a)
+// console.log(b)
+
+if(true){
+    const username="gautam"
+    if(username==="gautam"){
+        const web="github"
+        console.log(username+" "+web)
+    }
+    //console.log(web)
+}
+//console.log(username)
+
+function addNum(num){
+    return num+1
+}
+addNum(2)
+
+const addtwoNum=function(num){
+    return num+2
+}
+addtwoNum(4)
