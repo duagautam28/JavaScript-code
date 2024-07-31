a54255c2323983d8377a7883808910ef1cdf5c66
duagautam28 <147668577+duagautam28@users.noreply.github.com>
2024-07-31 07:13:08 +0000

    javascript

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+// Imediately Invoked Function
+
+(function chai(){
+    //named IIFE
+    console.log("database coonected")
+})();    // ; mandotaryv
+
+( (name)=> {
+    //unamed iife
+    console.log("hello "+name)
+})("gautam");  // ; mandotary
